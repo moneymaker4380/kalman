92f04abe92e4b48153408d358d6ef1b35d450bef
lazyguy328 <81511431+lazyguy328@users.noreply.github.com>
2021-04-12 18:38:00 +0800

    Update strategy.py

--- src/strategy.py
+++ src/strategy.py
@@ -3,3 +3,14 @@ import backtrader as bt
 class Strategy(bt.Strategy):
     def __int__(self):
         pass
+    
+cerebro = bt.Cerebro()
+data = bt.feeds.PandasData(dataname=df0,
+                           fromdate = datetime.datetime(2006, 1, 2),
+                           todate = datetime.datetime(2020, 12, 31)
+                          )
+cerebro.adddata(data) 
+cerebro.addstrategy(TestStrategy)
+cerebro.broker.set_cash(cash=10000000)
+cerebro.run()
+cerebro.plot() 

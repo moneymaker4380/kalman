f97b71619fb560dd64a43ae069cff2dbb906fd36
lazyguy328 <81511431+lazyguy328@users.noreply.github.com>
2021-04-12 18:41:26 +0800

    Update strategy.py

--- src/strategy.py
+++ src/strategy.py
@@ -1,10 +1,35 @@
 import backtrader as bt
 from datetime import datetime
+import numpy as np
 
 class Strategy(bt.Strategy):
     def __int__(self):
         pass
     
+from statsmodels.tsa.stattools import coint
+def find_cointegrated_pairs(data):
+    n = data.shape[1]
+    pvalue_matrix = np.ones((n, n))
+    keys = data.keys()
+    pairs = []
+    for i in range(n):
+        for j in range(i+1, n):
+            result = coint(data[keys[i]], data[keys[j]])
+            pvalue_matrix[i, j] = result[1]
+            if result[1] < 0.05:
+                pairs.append((keys[i], keys[j]))
+    return pvalue_matrix, pairs
+
+from statsmodels.tsa.stattools import adfuller
+spread = train.asset2 - model.params[0] * train.asset1
+adf = adfuller(spread, maxlag = 1)
+print('Critical Value = ', adf[0])
+# probablity critical values
+print(adf[4])
+
+def z_score(series):
+ return ((series - np.mean(series)) / np.std(series))
+
 cerebro = bt.Cerebro()
 data = bt.feeds.PandasData(dataname=df0,
                            fromdate = datetime.datetime(2006, 1, 2),

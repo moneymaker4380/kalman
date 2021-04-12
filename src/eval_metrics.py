1c535a89d2ce1908d6840919294ceaad603aca19
ray <raymondzone1495@gmail.com>
2021-04-12 15:41:53 +0800

    setup trial2

--- src/eval_metrics.py
+++ src/eval_metrics.py
@@ -3,3 +3,18 @@
 class EvalMetrics:
     def __init__(self):
         pass
+
+    def sharpe(self):
+        pass
+
+    def sortino(self):
+        pass
+
+    def calmar(self):
+        pass
+
+    def cvar(self):
+        pass
+
+    def md(self):
+        pass

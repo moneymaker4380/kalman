0d11e96f32ef1f236dde9657bf28b54905a29bb0
ray <raymondzone1495@gmail.com>
2021-04-12 18:30:59 +0800

    setup trial2

--- src/__init__.py
+++ src/__init__.py
@@ -2,11 +2,12 @@
 # testing
 ###
 
-import backtrader
-
-class Strat:
-    def __init__(self):
-        pass
+import backtrader as bt
+import numpy as np
+import pandas as pd
+from pykalman import KalmanFilter
+from datetime import datetime
+from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
     ...

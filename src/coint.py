cb1fbc93bbc7358aafe36688d6885a65bee2b205
lazyguy328 <81511431+lazyguy328@users.noreply.github.com>
2021-04-17 21:58:16 +0800

    Update coint.py

--- src/coint.py
+++ src/coint.py
@@ -32,9 +32,9 @@ class Coint:
         # eg spread = train.asset2 - model.params[0] * train.asset1
         adf = adfuller(spread, maxlag=1)
         #set maxlag = 0?
-        # or adf = adfuller(spread, autolag='BIC')
+        #or adf = adfuller(spread, autolag='BIC')
         print('ADF Statistic: ', adf[0])
         print('p-value: ', adf[1])
-        # critical values
+        #critical values
         print(adf[4])
         pass

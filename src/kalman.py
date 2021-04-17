375bd46fdf28d05e88f4c91da5641eee8bd4efb0
ray <raymondzone1495@gmail.com>
2021-04-17 18:06:02 +0800

    .

--- src/kalman.py
+++ src/kalman.py
@@ -30,7 +30,7 @@ class Kalman:
     def update(self, eNew, timestamp):
         self.error_df.append(pd.DataFrame(eNew, index=timestamp))
         obs = [1,self.error_df.shift(1).iloc[-1].squeeze()]
-        obs.extend([self.error_df.diff().shift(i).iloc[-1].squeeze for i in range(1,self.p_lags+1)])
+        obs.extend([self.error_df.diff().shift(i).iloc[-1].squeeze() for i in range(1,self.p_lags+1)])
         obs = np.array(obs)[np.newaxis]
-        new_m, new_cov =
+        new_m, new_cov = self.kf.filter_update()
         pass

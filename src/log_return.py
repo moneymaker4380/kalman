0694c83a18dac8897dda3bfc1eeef15eb626b148
raymondcwh <75136012+raymondcwh@users.noreply.github.com>
2021-04-18 22:37:07 +0800

    Update log_return.py

--- src/log_return.py
+++ src/log_return.py
@@ -10,7 +10,6 @@ class LogReturn(bt.Indicator):
     lines = ('logreturns',)
     plotinfo = dict(plot=False)
 
-
     def next(self):
         # This makes sure enough bars have passed before trying to calcualte the log return.
-        self.l.logreturns[0] = math.log(self.datas[0].close[0] / self.datas[0].close[-len(self.data)+1])
+        self.l.logreturns[0] = math.log(self.datas[0].open[0] / self.datas[0].open[-len(self.data) + 1])
